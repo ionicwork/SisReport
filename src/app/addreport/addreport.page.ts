b706edd5b59b29e304c8e0f9dca2474a7078f406
ionicwork <umairionic360@gmail.com>
2021-11-15 15:24:32 +0500

    validation at add report

--- src/app/addreport/addreport.page.ts
+++ src/app/addreport/addreport.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { FormGroup, Validators , FormControl , FormBuilder } from '@angular/forms';
 import firebase from 'firebase';
 import { DatahelperService } from '../provider/datahelper.service';
 @Component({
@@ -9,18 +10,31 @@ import { DatahelperService } from '../provider/datahelper.service';
 })
 export class AddreportPage implements OnInit {
   report:any=[];
-  constructor(public navCtrl:NavController,public dataHelper:DatahelperService) { }
+  public addReport:FormGroup ;
+  constructor(public navCtrl:NavController,public dataHelper:DatahelperService,public _fb:FormBuilder,) { }
 
   ngOnInit() {
+    this.addReport = this._fb.group({
+      title: ['', Validators.compose([
+        Validators.required,
+      ])],
+      description: ['', Validators.compose([
+        Validators.required,
+      ])],
+    
+    })
   }
   devBack(){
     this.navCtrl.navigateForward('userdailyreport');
   }
-  addReport(){
-    this.report.timeStamp=Number(new Date());
-    this.report.userUid=this.dataHelper.user.uid;
-    firebase.database().ref('reports').push(this.report);
 
-  }
+  addDailyReport(report){
+    debugger;
+    report.timeStamp=Number(new Date());
+    report.userUid=this.dataHelper.user.uid;
+    console.log(report);
+    firebase.database().ref('reports').push(this.report);
+ 
+ }
   
 }
